097c4d7fc9ae17384f69a5b20fdf4204b486ad2f
Maxim <maxime.axenoff@yandex.ru>
2021-09-29 20:03:28 +0500

    second

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,5 @@
 "use strict"
 
-// import GameSavingData, { Game, readGameSaving as loadGame, writeGameSaving as saveGame } from './game.js';
-
-// const game = new Game();
-// game.start();
-
 export function getColorHealth(array) {
     if (array.health > 50) {
         return 'healthy';
